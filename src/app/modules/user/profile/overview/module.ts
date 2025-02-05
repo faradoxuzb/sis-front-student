6cf6cb91bbcd79c8d07843910ed23973fb32ef02
Maruf <dvoryanin01041998@gmail.com>
2025-02-06 01:10:22 +0500

    add overview menu

---
+++ src/app/modules/user/profile/overview/module.ts
@@ -0,0 +1,40 @@
+export interface Schedule {
+    current_page: number;
+    data: schoolInfo[];
+    first_page_url: string;
+    from: number;
+    last_page: number;
+    last_page_url: string;
+    links: Link[];
+    next_page_url: null;
+    path: string;
+    per_page: number;
+    prev_page_url: null;
+    to: number;
+    total: number;
+}
+
+export interface schoolInfo {
+    id: number;
+    educationYear: EducationYear;
+    student: Student;
+    contract_number: string;
+    signing_date: null;
+    rejection_date: null;
+}
+
+export interface EducationYear {
+    id: number;
+    name: string;
+}
+
+export interface Student {
+    id: number;
+    fullName: string;
+}
+
+export interface Link {
+    url: null | string;
+    label: string;
+    active: boolean;
+}
